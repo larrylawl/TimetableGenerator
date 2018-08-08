836dcc444772167924689870cec576a484df6cbf
Larry Law <larrylaw97@gmail.com>
2018-08-08 23:39:29 +0800

    Added make_lecture_dict. Works as a function, but not integrated with scrapy

--- timetable/timetable/spiders/CORS_Scrapy.py
+++ timetable/timetable/spiders/CORS_Scrapy.py
@@ -20,10 +20,39 @@ class TimetableSpider(scrapy.Spider):
             yield scrapy.Request(next_url, callback = self.parse_detailed)
     
     def parse_detailed(self, response):
-        return {
-                "Module Code": response.xpath("normalize-space(//div[text()='Module Code :']/../../td[2])").extract()
-                "Module Title": response.xpath("//td[text()='Module Title :']/../td[2]/text").extract()
+        for cycle in range:
+                {
+                "Module Code": response.xpath("normalize-space(//div[text()='Module Code :']/../../td[2])").extract(),
+                "Module Title": response.xpath("//td[text()='Module Title :']/../td[2]/text").extract(),
                 "Exam Date": response.xpath("normalize-space(//td[text()='Exam Date :']/../td[2]/text())").extract() 
                 
                 }
-        #Definitely correct
+    def make_lecture_dict(lecture_list):
+        """
+        Input: List of lecture time table, unsorted by class
+        Output: Dictionary of lecture time table, sorted by class
+        """
+        for row in range(int(len(lecture_list)/7)): #for each row in Lecture Time Table
+            lecture_details["V"+str(row+1)] = {
+                                                "Class": lecture_list[0+(7*row)],
+                                                "Type": lecture_list[1+(7*row)],
+                                                "Week Type": lecture_list[2+(7*row)]
+                                                "Week Day": lecture_list[3+(7*row)]
+                                                "Start": lecture_list[4+(7*row)]
+                                                "End": lecture_list[5+(7*row)]
+                                                "Room": lecture_list[6+(7*row)]
+                                                }
+                            
+                            
+                            
+                
+                                                
+                
+{"Class": lecture_list[0+(7*row)],
+                       
+                
+                
+                
+                }
+            
+            
