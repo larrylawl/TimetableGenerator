e9a07eb32f4f5b5c8fcc560a3e405730e6f1917b
Larry Law <larrylaw97@gmail.com>
2018-08-08 23:47:17 +0800

    Integrated make_lecture_dict. might not work

--- timetable/timetable/spiders/CORS_Scrapy.py
+++ timetable/timetable/spiders/CORS_Scrapy.py
@@ -7,7 +7,6 @@ Created on Sat Aug  4 19:28:10 2018
 """
 
 import scrapy
-from scrapy.crawler import CrawlerProcess
 
 class TimetableSpider(scrapy.Spider):
     name = "timetable"
@@ -20,28 +19,32 @@ class TimetableSpider(scrapy.Spider):
             yield scrapy.Request(next_url, callback = self.parse_detailed)
     
     def parse_detailed(self, response):
-        for cycle in range:
-                {
+        lecture_list = response.xpath("//b[text()='Lecture Time Table']/../../../tr[2]/td/div/table/tr[position()>1]/td/text()").extract()
+        lecture_details = make_lecture_dict(lecture_list)
+        return {
                 "Module Code": response.xpath("normalize-space(//div[text()='Module Code :']/../../td[2])").extract(),
                 "Module Title": response.xpath("//td[text()='Module Title :']/../td[2]/text").extract(),
-                "Exam Date": response.xpath("normalize-space(//td[text()='Exam Date :']/../td[2]/text())").extract() 
-                
+                "Exam Date": response.xpath("normalize-space(//td[text()='Exam Date :']/../td[2]/text())").extract(),
+                "Lecture": lecture_details
                 }
+    
     def make_lecture_dict(lecture_list):
         """
         Input: List of lecture time table, unsorted by class
         Output: Dictionary of lecture time table, sorted by class
         """
+        lecture_dict = {}
         for row in range(int(len(lecture_list)/7)): #for each row in Lecture Time Table
-            lecture_details["V"+str(row+1)] = {
-                                                "Class": lecture_list[0+(7*row)],
-                                                "Type": lecture_list[1+(7*row)],
-                                                "Week Type": lecture_list[2+(7*row)]
-                                                "Week Day": lecture_list[3+(7*row)]
-                                                "Start": lecture_list[4+(7*row)]
-                                                "End": lecture_list[5+(7*row)]
-                                                "Room": lecture_list[6+(7*row)]
-                                                }
+            lecture_dict["V"+str(row+1)] = {
+                                            "Class": lecture_list[0+(7*row)],
+                                            "Type": lecture_list[1+(7*row)],
+                                            "Week Type": lecture_list[2+(7*row)],
+                                            "Week Day": lecture_list[3+(7*row)],
+                                            "Start": lecture_list[4+(7*row)],
+                                            "End": lecture_list[5+(7*row)],
+                                            "Room": lecture_list[6+(7*row)]
+                                            }
+        return lecture_dict
                             
                             
                             
